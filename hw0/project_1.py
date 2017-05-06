04cefcbf99712e929d68d918f191d8801f8ae7cd
Josh Mau <jjmau14@gmail.com>
2017-05-06 17:56:27 -0600

    init commit hw0

---
+++ hw0/project_1.py
@@ -0,0 +1,54 @@
+import random
+menu=True
+while menu:
+    print("""
+1.Flip A Coin 100 Times
+2.Fortune Cookie
+3.Pocket Color
+4.Number Of Hot Dogs
+5.Exit/Quit
+""")
+    menu=input("What Would You Like To Do? ")
+    if menu=="1":
+           
+        heads = 0
+        tails = 0
+        rounds = 0
+        while rounds < 100:
+            coin = random.randint(1, 2)
+            if coin == 1:
+                heads += 1
+                rounds += 1
+            elif coin == 2:
+                tails += 1
+                rounds += 1
+        print("You Flipped Heads {} Times!".format(heads))
+        print("You Flipped Tails {} Times!".format(tails))
+      
+    elif menu=="2":
+        fortune = random.randint(1, 5)
+        if fortune == 1:
+            print ("A beautiful, smart, and loving person will be coming into your life.")
+        
+        elif fortune == 2:
+            print ("A dubious friend may be an enemy in camouflage.")
+        
+        elif fortune == 3:
+            print ("A feather in the hand is better than a bird in the air.")
+        
+        elif fortune == 4:
+            print ("A fresh start will put you on your way.")
+        
+        elif fortune == 5:
+            print ("A friend asks only for your time not your money.")
+
+    elif menu=="3":
+        print ("hi")
+       
+    elif menu=="4":
+        print("hi")
+    elif menu=="5":
+        print("Goodbye")
+        menu = None
+    else:
+        print("Not Valid Choice Try again")
